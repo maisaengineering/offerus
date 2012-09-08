6ba5a749f3cbffbd0d23bbb0ca7bfe04b63ee7bf
sukendhar <sukendhar555@gmail.com>
2012-09-08 13:26:20 +0530

    javascript files added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :fname, :lname, :email, :password, :password_confirmation, :remember_me, :confirmed_at, :provider, :uid, :token, :image_url, :role, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email
   # attr_accessible :title, :body
+  validates_presence_of :fname, :email  
+  validates_presence_of :password, :on => :create  
+  validates_presence_of :fname, :with => /^[-\w\._@]+$/i, :message => "should only contain letters, numbers, or .-_@"  
+  validates_length_of :password, :minimum => 4
+  validates_confirmation_of :password  
+
   def facebook
     FbGraph::User.new('me', :access_token => User.current_user.token).fetch
   end
